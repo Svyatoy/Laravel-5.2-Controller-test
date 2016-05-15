35d21f89105dc8e497a1b085b336d9f62be12e8e
svyatoy <kreyser2007@mail.ru>
2016-05-15 22:47:51 +0300

    Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,35 +1,44 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('home', function () {
-    return phpinfo();
-});
-
 Route::group(['prefix' => 'api/v1.1'], function () {
+    /**
+     * Password reset routes
+     */
+        Route::post('reset', 'PasswordResetController@store');
+        Route::put('reset', 'PasswordResetController@update');
 
-    Route::post('reset', 'PasswordResetController@store');
-    Route::put('reset', 'PasswordResetController@update');
+    /**
+     * Authentication routes
+     */
+        Route::post('authenticate', 'TokenController@authenticate');
+        Route::get('refresh', 'TokenController@refresh');
+        Route::get('logout', 'TokenController@logout');
+        Route::get('authenticate/user', 'TokenController@getAuthenticatedUser');
 
-    Route::post('authenticate', 'TokenController@authenticate');
-    Route::get('refresh', 'TokenController@refresh');
-    Route::get('logout', 'TokenController@logout');
-    Route::get('authenticate/user', 'TokenController@getAuthenticatedUser');
+    /**
+     * Users objects routes
+     */
+        Route::resource('users','UserController',['only' =>[
+            'index', 'show', 'update', 'destroy', 'store'
+        ]]);
 
-    Route::resource('users','UserController',['only' =>[
-        'index', 'show', 'update', 'destroy', 'store'
-    ]]);
-    Route::get('users/{users}/albums', 'UserController@getUserAlbums');
+        // Get the user albums list
+        Route::get('users/{users}/albums', 'UserController@getUserAlbums');
 
-    Route::resource('albums', 'AlbumController', ['only' =>[
-        'index', 'store', 'show', 'update', 'destroy'
-    ]]);
+    /**
+     * Albums objects routes
+     */
+        Route::resource('albums', 'AlbumController', ['only' =>[
+            'index', 'store', 'show', 'update', 'destroy'
+        ]]);
 
-    Route::resource('photos', 'PhotoController', ['only' =>[
-        'index', 'store', 'show', 'destroy'
-    ]]);
-    Route::get('photos/{photo}/resized', 'PhotoController@resized');
+    /**
+     * Photos object routes
+     */
+        Route::resource('photos', 'PhotoController', ['only' =>[
+            'index', 'store', 'show', 'destroy'
+        ]]);
 
+        // Get the miniatures of photo
+        Route::get('photos/{photo}/resized', 'PhotoController@resized');
 });
